b7b5ec8cfdd3de758780f9928dd3c8af812c091c
RuslanTenderyts <rylya1995@gmail.com>
2023-03-21 16:04:39 +0200

    BrowserRouter

--- src/page/Movies.js
+++ src/page/Movies.js
@@ -1,31 +1,35 @@
-import { Search } from "components/Search/Search"
-import { useState, useEffect } from "react"
-import { fetchMoviesWithQuery } from "service/api" 
-import { MoviesList } from "components/MoviesList/MoviesList"
+import { Search } from "components/Search/Search";
+import { useSearchParams } from "react-router-dom" ;
+import { useState, useEffect } from "react";
+import { fetchMoviesWithQuery } from "service/api";
+import { MoviesList } from "components/MoviesList/MoviesList";
 
 export const Movies = () => {
-    const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const movieName = searchParams.get("query") ?? "";
     
     useEffect(() => { 
-        if(!query){
+        if(!movieName){
             return;
         }         
-        async function getFetch (query) {
+        async function getFetch (movieName) {
             try {
-                const data = await fetchMoviesWithQuery(query);
+                const data = await fetchMoviesWithQuery(movieName);
                 setMovies( data.results );  
                 console.log( data.results)           
             } catch (error) {
                 console.log(error)
             } 
         };
-        getFetch(query);
-      }, [query] );
+        getFetch(movieName);
+      }, [ movieName ] );
 
-    const handleSubmit = (value) => {
-        setQuery(value)
+    const handleSubmit = (query) => {
+        const nextParams = query !== "" ? { query } : {};
+        setSearchParams(nextParams);
     }
+
     return (
         <div>
             <Search onSubmit={handleSubmit} />
